4cf2e18793e4b185b8069980fd53c19292609a03
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2020-06-19 23:30:55 +0300

    update

---
+++ precache-manifest.c620e14879a95d8a971f7587f4b3ea9c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c5209b0c825cbe5e6dca1b464134ef7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28054b4eeb6c8bbc6fde",
+    "url": "/static/css/main.dd5463d0.chunk.css"
+  },
+  {
+    "revision": "d2dd75aea2edb84e814f",
+    "url": "/static/js/2.fcadefc6.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.fcadefc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28054b4eeb6c8bbc6fde",
+    "url": "/static/js/main.9140134f.chunk.js"
+  },
+  {
+    "revision": "fe4c07bf31cdd9ea6259",
+    "url": "/static/js/runtime-main.c5c94ff6.js"
+  },
+  {
+    "revision": "5048d7fd453dc66852198c2ea12345f6",
+    "url": "/static/media/dancing.5048d7fd.gif"
+  },
+  {
+    "revision": "8390f7e711ec9cf2c6adf05f029d4caf",
+    "url": "/static/media/egg-shake.8390f7e7.gif"
+  },
+  {
+    "revision": "46bc27d5e21e0b9cfce7807b4db173dd",
+    "url": "/static/media/falls.46bc27d5.gif"
+  },
+  {
+    "revision": "5b77e3f2014ad6a134f9eb3467992f32",
+    "url": "/static/media/hearts-1.5b77e3f2.gif"
+  },
+  {
+    "revision": "2d544598adc7b46833a05cd9401b7dbb",
+    "url": "/static/media/hearts-2.2d544598.gif"
+  },
+  {
+    "revision": "89e5e36aaecb276d4f68a8f3cd7d9ed4",
+    "url": "/static/media/monkeys.89e5e36a.gif"
+  },
+  {
+    "revision": "60a7cae8d00392a57b1af7293665e7c7",
+    "url": "/static/media/wat.60a7cae8.gif"
+  }
+]);
